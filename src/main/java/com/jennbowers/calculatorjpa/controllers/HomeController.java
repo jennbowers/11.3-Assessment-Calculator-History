2ff1b65b9d5c8bbed6da62e094ee49672dc60bd4
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-02 16:29:18 -0400

    Set up userhistory html to display all operations history on load, and added form to search for specific user's operations.

--- src/main/java/com/jennbowers/calculatorjpa/controllers/HomeController.java
+++ src/main/java/com/jennbowers/calculatorjpa/controllers/HomeController.java
@@ -5,8 +5,10 @@ import com.jennbowers.calculatorjpa.models.Operations;
 import com.jennbowers.calculatorjpa.repositories.OperationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -39,6 +41,15 @@ public class HomeController {
         }
         return "index";
     }
+
+    @RequestMapping(value = "/history", method = RequestMethod.GET)
+    public String history(Model model) {
+        Iterable<Operations> operations = repo.findAll();
+        model.addAttribute("operations", operations);
+        return "userHistory";
+    }
+
+
 
 }
 
